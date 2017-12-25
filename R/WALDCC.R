4afdcf2220608b08a58b7f00392530f0f3cc82a5
wikithink <wikithink@qq.com>
2017-12-25 10:42:19 +0800

    Update WALDCC.R

--- R/WALDCC.R
+++ R/WALDCC.R
@@ -1,6 +1,7 @@
-WALDCC <-
-function(inpmat,alpha)
-{
+#修改了WALDCC函数的输出格式
+#王文祥
+#2017-12-22
+WALDCC <- function(inpmat,alpha){
 k = length(inpmat)
 s = sum(inpmat)
 chi = qchisq(1-alpha, df=1)
@@ -16,16 +17,22 @@ if (WALDCC.UL[r] > 1) ULA[r] = 1 else ULA[r]=WALDCC.UL[r]
 }
 diA=ULA-LLA##FIND LENGTH OF CIs
 VOL=round(prod(diA),8)##PRODUCT OF LENGTH OF CIs
-cat('Original Intervals\n')
-cat('Lower Limit\n')
-print(WALDCC.LL)
-cat('Upper Limit\n')
-print(WALDCC.UL)
-cat('Adjusted Intervals\n')
-cat('Lower Limit\n')
-print(LLA)
-cat('Upper Limit\n')
-print(ULA)
-cat('Volume\n')
-print(VOL)
+# cat('Original Intervals\n')
+# cat('Lower Limit\n')
+# print(WALDCC.LL)
+# cat('Upper Limit\n')
+# print(WALDCC.UL)
+# cat('Adjusted Intervals\n')
+# cat('Lower Limit\n')
+# print(LLA)
+# cat('Upper Limit\n')
+# print(ULA)
+# cat('Volume\n')
+# print(VOL)
+#转成数据框
+method <- 'Wald with continuity corrections'
+waldcccl <- as.data.frame(cbind(inpmat,round(pi,4),alpha,1-alpha,WALDCC.LL,WALDCC.UL,LLA,ULA,method))
+names(waldcccl) <- c("x","prop","alpha","conf","LL","UL","LLA","ULA","method")
+u <- list(waldcccl=waldcccl,vol=VOL)
+return(u)
 }
